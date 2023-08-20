7dae0b390bc6e46994e002a8be53cbd22644851d
Wei Lei <lei.wei@thoughtworks.com>
2023-08-20 23:19:31 +0800

    add some new controller methods

--- src/main/java/com/example/spring6reactive/controllers/BeerController.java
+++ src/main/java/com/example/spring6reactive/controllers/BeerController.java
@@ -3,12 +3,16 @@ package com.example.spring6reactive.controllers;
 
 import com.example.spring6reactive.domain.Beer;
 import com.example.spring6reactive.services.BeerService;
+import jakarta.validation.constraints.Null;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
@@ -36,5 +40,16 @@ public class BeerController {
                 UriComponentsBuilder.fromHttpUrl("http://localhost:8080/beers/" + newBeer.getId())
                         .build().toUri()
         ).build());
+    }
+
+    @PutMapping("/beers/{beerId}")
+    public ResponseEntity<Void> updateBeer(@PathVariable("beerId") Integer beerId, @RequestBody Beer beer) {
+        this.beerService.updateBeer(beerId, beer).subscribe();
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/beers/{beerId}")
+    public Mono<ResponseEntity<Void>> deleteBeerById(@PathVariable Integer beerId) {
+        return this.beerService.deleteById(beerId).map((beer) -> ResponseEntity.noContent().build());
     }
 }
--- src/main/java/com/example/spring6reactive/services/BeerService.java
+++ src/main/java/com/example/spring6reactive/services/BeerService.java
@@ -3,6 +3,7 @@ package com.example.spring6reactive.services;
 import com.example.spring6reactive.domain.Beer;
 import com.example.spring6reactive.repositories.BeerRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -22,5 +23,18 @@ public class BeerService {
 
     public Mono<Beer> createBeer(Beer beer) {
         return this.beerRepository.save(beer);
+    }
+
+    public Mono<Beer> updateBeer(Integer beerId, Beer beer) {
+        return this.getById(beerId).map(foundBeer -> {
+            foundBeer.setBeerName(beer.getBeerName());
+            foundBeer.setBeerStyle(beer.getBeerStyle());
+            return foundBeer;
+        }).flatMap(this.beerRepository::save);
+
+    }
+
+    public Mono<Void> deleteById(Integer beerId) {
+        return this.beerRepository.deleteById(beerId);
     }
 }
